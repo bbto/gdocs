c5311f723cd5afc70e7438b370849ddb44937614
Alvaro Insignares <alvaro@koombea.com>
2011-09-29 23:57:11 -0500

    Override method missing in XmlParser class

--- lib/gdocs/objects/xml_parser.rb
+++ lib/gdocs/objects/xml_parser.rb
@@ -6,6 +6,23 @@ module Gdocs
       def parse(xml_string)
 
       end
+      
+      # Redefining 
+      def method_missing(m, *args, &block)
+        if attributes.include?(m.to_s.gsub("=",""))
+          if m.to_s.include?("=")
+            self.attributes[m.to_s.gsub("=","")] = *args.to_s
+          else
+            result = self.attributes[m.to_s]
+          end
+        else
+          raise NoMethodError.new("Method missing #{m}")
+        end
+      end
+      
+      def attributes
+        return []
+      end
     end
   end
 end
